b5a3474866c6c84e34b17f75f3fb38c7d9b26c0f
caijun_yang <yangcaijun@126.com>
2018-03-28 16:51:55 +0800

    all

---
+++ manage/src/main/java/com/itplayer/DemoApplication.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.itplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author zhailiang
+ *
+ */
+@SpringBootApplication
+//@RestController
+//@EnableSwagger2
+public class DemoApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+//	@GetMapping("/hello")
+	public String hello() {
+		return "hello spring security";
+	}
+
+}
